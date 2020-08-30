7069186d35f20f0ea5e75393d2804df2e5c57d6f
omar badawy <omarbadawy10022@gmail.com>
2020-08-30 12:28:14 +0200

    build

---
+++ build/precache-manifest.e03b7df2586f7f6c31bf53fa0292f840.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a64b4226c3aac1af51cc3b5f7fd693",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c621c540bf8de83d21a",
+    "url": "/static/css/main.42dafb61.chunk.css"
+  },
+  {
+    "revision": "669689b92c49792cca6f",
+    "url": "/static/js/2.81bcb34d.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/static/js/2.81bcb34d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c621c540bf8de83d21a",
+    "url": "/static/js/main.19d84847.chunk.js"
+  },
+  {
+    "revision": "2e075117383e7b156060",
+    "url": "/static/js/runtime-main.2f0bb968.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/logo.d7265326.png"
+  }
+]);
